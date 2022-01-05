510f7d6deb76312d93214a2c1d0feecc5489aaa8
Mohamed Tanash <tanashkoo@gmail.com>
2022-01-05 22:24:52 +0200

    Building our React app

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,7 @@
 const config = {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY:
+    "pk_test_51K5oedF4Abdf3ZQAZuHaXgSMC0Q3nkG42bCbzS6UlzWujTNxpOr4NpUxCErQh9KFXkaOHgk9ZBWoOAGkyDLOIPBc00O5wj6Jv7",
   // Backend config
   s3: {
     REGION: process.env.REACT_APP_REGION,
